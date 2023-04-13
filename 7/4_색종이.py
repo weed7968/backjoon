950edcdf817b5d76700326cee68012a2df3f3f90
weed7968 <tlstjdcjf980809@gmail.com>
2023-04-13 17:44:17 +0900

    23-04-13

---
+++ 7/4_색종이.py
@@ -0,0 +1,14 @@
+arr = [[0]*100 for _ in range(100)]
+
+n = int(input())
+for _ in range(n):
+    x,y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1
+
+count = 0
+for k in range(100):
+    count += arr[k].count(1)
+
+print(count)
